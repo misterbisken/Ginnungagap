4e69ec4862600a92f6d3ca86672f4624304867f9
Misterbisken <zorro.andree@hotmail.com>
2026-05-06 20:57:04 +0200

    Hello world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event;
+
+fn main() -> std::io::Result<()> {
+    ratatui::run(|terminal| {
+        loop {
+            terminal.draw(|frame| {
+                frame.render_widget("Hello!", frame.area());
+            })?;
+            if event::read()?.is_key_press() {
+                break Ok(());
+            }
+        }
+    })
 }
